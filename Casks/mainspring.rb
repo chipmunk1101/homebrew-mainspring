ba1a428c93ad0cd16682a76f92ed0fd1ff2cfef5
chipmunk1101 <chipmunk1101@users.noreply.github.com>
2026-07-25 23:36:11 +0530

    cask: add livecheck, verified url, refined desc

--- Casks/mainspring.rb
+++ Casks/mainspring.rb
@@ -2,10 +2,18 @@ cask "mainspring" do
   version "3.0.1"
   sha256 "a9927f0758fd02f65fcdd5c2c8b05a1411cf4275a31f914486cdfd309fb914d8"
 
-  url "https://trymainspring.com/downloads/Mainspring-#{version}.pkg"
+  url "https://trymainspring.com/downloads/Mainspring-#{version}.pkg",
+      verified: "trymainspring.com/"
   name "Mainspring"
-  desc "Turn 90+ hidden macOS settings into one-click, reversible toggles"
+  desc "Utility for reversibly toggling hidden system settings"
   homepage "https://trymainspring.com/"
+
+  livecheck do
+    url "https://trymainspring.com/downloads/Mainspring.pkg"
+    strategy :header_match do |headers|
+      headers["content-disposition"][/Mainspring[._-]v?(\d+(?:\.\d+)+)\.pkg/i, 1]
+    end
+  end
 
   depends_on macos: :ventura
 
